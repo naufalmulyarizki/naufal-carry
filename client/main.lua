2615a1c620f2a0335de38756eb2de22739bed238
Naufal Mulyarizki <95608072+naufalmulyarizki@users.noreply.github.com>
2025-10-28 14:01:57 +0700

    Validasi tambahan tidak bisa menggunakan command /carry ketika tidak ada player disektiar

--- client/main.lua
+++ client/main.lua
@@ -4,19 +4,20 @@ for k,v in pairs(Config.Command) do
     RegisterCommand(k, function(source, args)   
         local coords = GetEntityCoords(cache.ped)
         local closestPlayer = lib.getNearbyPlayers(coords, 3.0)
-        local target = GetPlayerServerId(closestPlayer[1].id)
-        
-        if not LocalPlayer.state.isLoggedIn then return end
-        if not carryingBackInProgress then
-            TriggerServerEvent('naufal-carry:server:sendRequest', target, closestPlayer, v.libanim1, v.anim1, v.libanim2, v.anim2, v.distans, v.distans2, v.height, v.spin, v.length, v.controlFlagMe, v.controlFlagTarget, v.animFlagTarget)
-        else
-            carryingBackInProgress = false
-            ClearPedSecondaryTask(PlayerPedId())
-            DetachEntity(PlayerPedId(), true, false)
-            if target ~= 0 then 
-                TriggerServerEvent("naufal-carry:server:stop",target)
-            end
-        end
+		if #closestPlayer > 0 then
+	        local target = GetPlayerServerId(closestPlayer[1].id)
+	        if not LocalPlayer.state.isLoggedIn then return end
+	        if not carryingBackInProgress then
+	            TriggerServerEvent('naufal-carry:server:sendRequest', target, closestPlayer, v.libanim1, v.anim1, v.libanim2, v.anim2, v.distans, v.distans2, v.height, v.spin, v.length, v.controlFlagMe, v.controlFlagTarget, v.animFlagTarget)
+	        else
+	            carryingBackInProgress = false
+	            ClearPedSecondaryTask(PlayerPedId())
+	            DetachEntity(PlayerPedId(), true, false)
+	            if target ~= 0 then 
+	                TriggerServerEvent("naufal-carry:server:stop",target)
+	            end
+	        end
+		end
     end)
 end
 
